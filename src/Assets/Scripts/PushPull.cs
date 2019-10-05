ab5af69349b10046a4b07c1925547986906922d8
Cbrichardson6 <44329476+Cbrichardson6@users.noreply.github.com>
2019-10-05 10:01:52 -0700

    Grab Objects now possible
    Can now grab objects and push them, but no limitations on normal movement yet.

---
+++ src/Assets/Scripts/PushPull.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPull : MonoBehaviour
+{
+
+    private GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update() {
+        if (Input.GetButtonUp("Grab") && transform.parent != null) {
+            transform.parent = null;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.CompareTag("Player") && Input.GetButtonDown("Grab")) {
+            transform.SetParent(player.transform);
+        }
+    }
+}
