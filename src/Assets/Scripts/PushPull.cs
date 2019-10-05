9fc6b48b765208a6cdf0bc5e6607e01e7c7877fe
Cbrichardson6 <44329476+Cbrichardson6@users.noreply.github.com>
2019-10-05 12:03:01 -0700

    Changes to Push/Pull
    Push and Pull objects now have gravity, and it is not possible to grab while not grounded.

--- src/Assets/Scripts/PushPull.cs
+++ src/Assets/Scripts/PushPull.cs
@@ -6,15 +6,18 @@ public class PushPull : MonoBehaviour
 {
 
     private GameObject player;
+    private Rigidbody2D rb;
 
     void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
         if (Input.GetButtonUp("Grab") && transform.parent != null) {
             transform.parent = null;
             player.GetComponent<playerMovement>().holding = false;
+            rb = this.gameObject.AddComponent<Rigidbody2D>();
         }
     }
 
@@ -23,7 +26,7 @@ public class PushPull : MonoBehaviour
             && player.GetComponent<CharacterController2D>().m_Grounded) {
             transform.SetParent(player.transform);
             player.GetComponent<playerMovement>().holding = true;
-            
+            Destroy(rb);
         }
     }
 }
