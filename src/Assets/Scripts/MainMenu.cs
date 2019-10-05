7b826ccf26a244fec5b022ff65dfb9cfc8b07585
Benjamin Perumala <benperumala@users.noreply.github.com>
2019-10-05 11:50:21 -0700

    Main Menu work
    Except nothing actually works

---
+++ src/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.SceneManager;
+
+public class MainMenu : MonoBehaviour {
+    // https://youtu.be/zc8ac_qUXQY
+
+    public void PlayGame() {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+    }
+}
